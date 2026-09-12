86f4ecb64926677c08a0b8599e28480572f79460
giovanni-trevisan <giovannitrevisan0305@gmail.com>
2026-09-12 22:36:58 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-const C='chronomap-v10';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./data.json','./territories.geojson','./sources.json','./manifest.webmanifest','./icon.svg']))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{let y=x.clone();caches.open(C).then(c=>c.put(e.request,y));return x}).catch(()=>caches.match('./index.html')))));
+const CACHE='chronomap-v12';
+const ASSETS=['./','./index.html','./data.json','./territories.geojson','./sources.json','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const y=x.clone();caches.open(CACHE).then(c=>c.put(e.request,y));return x;}))));
