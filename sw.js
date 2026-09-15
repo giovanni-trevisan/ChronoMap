6f5222565388ab7258137eb08e7bbcf2327e321b
giovanni-trevisan <giovannitrevisan0305@gmail.com>
2026-09-15 14:35:22 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='chronomap-v13-5';
+const CACHE='chronomap-v13-5-clean';
 const ASSETS=['./','./index.html','./data.json','./territories.geojson','./sources.json','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
