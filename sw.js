c147c4b77efb6fa752e737a81ac5d961d1058064
giovanni-trevisan <giovannitrevisan0305@gmail.com>
2026-09-16 20:31:37 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='chronomap-v13-7';
-const ASSETS=['./','./index.html','./data.json','./territories.geojson','./sources.json','./manifest.webmanifest','./icon.svg'];
+const CACHE='chronomap-avignon-test-1';
+const ASSETS=['./','./index.html','./data.json','./sources.json','./territories.geojson','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const y=x.clone();caches.open(CACHE).then(c=>c.put(e.request,y));return x;}))));
